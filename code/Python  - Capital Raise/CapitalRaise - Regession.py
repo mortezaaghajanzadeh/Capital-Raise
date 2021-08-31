feca5ebd35aa5d27a946251070dfaa6d182cbf4c
mortezaaghajaznadeh <mortezaaghajanzadeh@gmail.com>
2021-08-31 18:43:08 +0430

    v1

--- code/Python  - Capital Raise/CapitalRaise - Regession.py
+++ code/Python  - Capital Raise/CapitalRaise - Regession.py
@@ -9,5 +9,16 @@ Data = pd.read_parquet(d)
 list(Data)
 # %%
 df = Data[['name','jalaliDate','IndlImbalance',
- 'InslImbalance','CAR','t','EPeriod']]
+ 'InslImbalance','CAR','t','EPeriod','nEvent']]
+
+mapdf  = df.groupby(['t']).IndlImbalance.mean().to_frame()
+mapdict = dict(zip(mapdf.index,mapdf['IndlImbalance']))
+df['IndNT'] = df.t.map(mapdict)
+df['IndNT'] = df.IndlImbalance - df.IndNT
+
+
+mapdf  = df.groupby(['t']).InslImbalance.mean().to_frame()
+mapdict = dict(zip(mapdf.index,mapdf['InslImbalance']))
+df['InsNT'] = df.t.map(mapdict)
+df['InsNT'] = df.InslImbalance - df.IndNT
 df
