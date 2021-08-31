f1b2edc9ddd70f135dc7568e2e43a2704a4865fb
mortezaaghajaznadeh <mortezaaghajanzadeh@gmail.com>
2021-08-31 18:10:47 +0430

    v1

---
+++ code/Python  - Capital Raise/CapitalRaise - Abnormal Return.py
@@ -0,0 +1,351 @@
+# %%
+from AB_Functions import *
+from CleaningFunctions import *
+path = r"G:\Economics\Finance(Prof.Heidari-Aghajanzadeh)\Data\Capital Rise\\"
+path1 = r"G:\Economics\Finance(Prof.Heidari-Aghajanzadeh)\Data\\"
+#%%
+pdf = pd.read_parquet(path1 + "Cleaned_Stocks_Prices_1400-04-27.parquet").reset_index(drop = True)
+
+
+#%%
+indexes = pd.read_csv(path1 + "IndustryIndexes_1400-04-27.csv").reset_index(drop = True)
+for i in ['industry_index','industry_size']:
+    mapdict = dict(zip(indexes.set_index(['group_id','date']).index,indexes[i]))
+    pdf[i] = pdf.set_index(['group_id','date']).index.map(mapdict)
+    
+#%%
+gg = pdf.groupby(['date','group_id'])
+pdf = pdf.set_index(['date','group_id'])
+pdf['Weight'] = gg.MarketCap.sum()
+pdf['Weight'] = pdf.MarketCap / pdf.Weight
+pdf = pdf.reset_index().sort_values(by = ['name','date'])
+
+
+#%%
+
+df2 = pd.read_csv(path + "Stock_price_trade_1387_1400" + ".csv")
+mlist = ['stock_id','date',
+    'ind_buy_volume',
+ 'ins_buy_volume',
+ 'ind_buy_value',
+ 'ins_buy_value',
+ 'ins_buy_count',
+ 'ind_buy_count',
+ 'ind_sell_volume',
+ 'ins_sell_volume',
+ 'ind_sell_value',
+ 'ins_sell_value',
+ 'ins_sell_count',
+ 'ind_sell_count'
+]
+print(len(pdf), len(df2))
+
+i = 'stock_id'
+df2[i] = df2[i].astype(str)
+pdf = pdf.merge(df2[mlist], on=["stock_id", "date"], how="left").drop_duplicates()
+print(len(pdf))
+pdf = pdf.rename(columns = {'close_price' : "UnadjustedPrice",
+                            "AdjustedPrice":"close_price"})    
+    
+#%%
+df = pd.read_excel(path + "Capital Rise - 71-99.xlsx")
+df = df[df.CapAfter != df.CapBefore]
+df = df[~df.Symbol.isnull()].rename(columns={"Symbol": "name"})
+df = df[
+    [
+        "name",
+        "year",
+        "Firm",
+        "CapAfter",
+        "CapBefore",
+        "ExtOrdGMDate",
+        "Revaluation",
+        "JustRO",
+        "JustSaving",
+        "JustPremium",
+        "JustRevaluation",
+        "Hybrid",
+        "%CapRaised",
+        "%PremiumCapRaising",
+        "%ROCapRaising",
+        "%SavingCapRaising",
+    ]
+]
+
+
+df["CapBefore2"] = (df["CapBefore"] / 10).round(0)
+df["CapAfter2"] = (df["CapAfter"] / 10).round(0)
+df["ExtOrdGMDate2"] = (df["ExtOrdGMDate"] / 10).round(0)
+
+df = df.drop_duplicates(subset=["name", "CapAfter2", "CapBefore2"], keep="first")
+df = df.drop_duplicates(subset=["ExtOrdGMDate2", "CapBefore2"], keep="first")
+df = df.drop_duplicates(subset=["ExtOrdGMDate2", "CapAfter2"], keep="first")
+df = df.drop_duplicates(subset=["ExtOrdGMDate", "CapBefore2"], keep="first")
+df = df.drop_duplicates(subset=["ExtOrdGMDate", "CapAfter2"], keep="first")
+
+df = df[df.CapBefore != 0]
+df["Percent"] = ((df["CapAfter"] - df["CapBefore"]) / df["CapBefore"]) * 100
+df["Percent"] = df["Percent"].round(2)
+df = df[df.Percent > 0]
+
+df = df[
+    [
+        "name",
+        "year",
+        "Firm",
+        "CapAfter",
+        "CapBefore",
+        "ExtOrdGMDate",
+        "Revaluation",
+        "JustRO",
+        "JustSaving",
+        "JustPremium",
+        "JustRevaluation",
+        "Hybrid",
+        "%CapRaised",
+        "%PremiumCapRaising",
+        "%ROCapRaising",
+        "%SavingCapRaising",
+    ]
+]
+df = df.drop_duplicates(subset=["name", "year", "CapBefore"], keep="first")
+df = df.drop_duplicates(subset=["name", "year", "CapAfter"], keep="first")
+
+df = df.drop_duplicates(subset=["ExtOrdGMDate", "name"], keep="first")
+
+
+df = df.sort_values(by=["name", "ExtOrdGMDate"])
+df = df[
+    [
+        "name",
+        "year",
+        "Firm",
+        "CapAfter",
+        "CapBefore",
+        "ExtOrdGMDate",
+        "Revaluation",
+        "JustRO",
+        "JustSaving",
+        "JustPremium",
+        "JustRevaluation",
+        "Hybrid",
+        "%CapRaised",
+        "%PremiumCapRaising",
+        "%ROCapRaising",
+        "%SavingCapRaising",
+    ]
+]
+df = df[(df.year > 1382) & (df.year < 1399)]
+df.head()
+
+# %%
+
+
+# %%
+mdf = pd.DataFrame()
+mdf = mdf.append(pdf)
+mdf = mdf[mdf.volume != 0]
+JalaliDates = list(set(mdf.jalaliDate))
+JalaliDates.sort()
+t = list(range(len(JalaliDates)))
+mapingdict = dict(zip(JalaliDates, t))
+mdf["t"] = mdf["jalaliDate"].map(mapingdict)
+mapingdict = dict(zip(JalaliDates, t))
+
+df["t"] = df["ExtOrdGMDate"].map(mapingdict)
+tIndex = pd.DataFrame(list(mapingdict.keys()), columns=[0])
+for i in set(df[df.t.isnull()].ExtOrdGMDate):
+    mapingdict[i] = tIndex[tIndex[0] >= i].index[0]
+df["t"] = df["ExtOrdGMDate"].map(mapingdict)
+
+
+# %%
+data = pd.DataFrame()
+ndf = pd.DataFrame()
+NotCapitalData = []
+gg = mdf.groupby("name")
+for symbol in list(gg.groups.keys()):
+    print(symbol)
+    d = gg.get_group(symbol)
+    if len(d) < 30:
+        continue
+    t = df[df.name == symbol]
+    if len(t) == 0:
+        NotCapitalData.append(symbol)
+        continue
+    g = pd.DataFrame()
+    g = g.append(d)
+    g["CapBefore"] = np.nan
+    g["CapAfter"] = np.nan
+    g["ExtOrdGMDate"] = np.nan
+    g["Event"] = np.nan
+    g["JustRO"] = np.nan
+    g["JustSaving"] = np.nan
+    g["JustPremium"] = np.nan
+    g["Hybrid"] = np.nan
+    g["Revaluation"] = np.nan
+    nEvent = 0
+    for date in t.ExtOrdGMDate:
+
+        Day = df[(df.name == symbol) & (df.ExtOrdGMDate == date)].ExtOrdGMDate.iloc[0]
+        CapBefore = df[(df.name == symbol) & (df.ExtOrdGMDate == date)].CapBefore.iloc[
+            0
+        ]
+        CapAfter = df[(df.name == symbol) & (df.ExtOrdGMDate == date)].CapAfter.iloc[0]
+        JustRO = df[(df.name == symbol) & (df.ExtOrdGMDate == date)].JustRO.iloc[0]
+        JustSaving = df[
+            (df.name == symbol) & (df.ExtOrdGMDate == date)
+        ].JustSaving.iloc[0]
+        JustPremium = df[
+            (df.name == symbol) & (df.ExtOrdGMDate == date)
+        ].JustPremium.iloc[0]
+        Hybrid = df[(df.name == symbol) & (df.ExtOrdGMDate == date)].Hybrid.iloc[0]
+        Revaluation = df[
+            (df.name == symbol) & (df.ExtOrdGMDate == date)
+        ].Revaluation.iloc[0]
+        mark = 0
+        #         print(g.jalaliDate.iloc[0],date)
+        if (date >= g.jalaliDate.iloc[0]) and (date < g.jalaliDate.iloc[-1]):
+            nEvent += 1
+            mark = 1
+            g.loc[g.jalaliDate >= date, "ExtOrdGMDate"] = Day
+            g.loc[g.jalaliDate >= date, "CapBefore"] = CapBefore
+            g.loc[g.jalaliDate >= date, "CapAfter"] = CapAfter
+            g.loc[g.jalaliDate >= date, "Event"] = g.loc[g.jalaliDate >= date].t.iloc[0]
+            g.loc[g.jalaliDate >= date, "JustRO"] = JustRO
+            g.loc[g.jalaliDate >= date, "JustSaving"] = JustSaving
+            g.loc[g.jalaliDate >= date, "JustPremium"] = JustPremium
+            g.loc[g.jalaliDate >= date, "Hybrid"] = Hybrid
+            g.loc[g.jalaliDate >= date, "Revaluation"] = Revaluation
+            if nEvent == 1:
+                g.loc[g.jalaliDate < date, "ExtOrdGMDate"] = Day
+                g.loc[g.jalaliDate < date, "CapBefore"] = CapBefore
+                g.loc[g.jalaliDate < date, "CapAfter"] = CapAfter
+                g.loc[g.jalaliDate < date, "Event"] = g.loc[
+                    g.jalaliDate >= date
+                ].t.iloc[0]
+                g.loc[g.jalaliDate < date, "JustRO"] = JustRO
+                g.loc[g.jalaliDate < date, "JustSaving"] = JustSaving
+                g.loc[g.jalaliDate < date, "JustPremium"] = JustPremium
+                g.loc[g.jalaliDate < date, "Hybrid"] = Hybrid
+                g.loc[g.jalaliDate < date, "Revaluation"] = Revaluation
+
+        if mark == 1:
+            ndf = ndf.append(df[(df.name == symbol) & (df.ExtOrdGMDate == date)])
+    if mark == 1:
+        data = data.append(g)
+
+ndf.to_excel(path + "SDate.xlsx", index=False)
+
+#%%
+
+
+# %%
+#
+data2 = data.sort_values(by=["name", "t"], ascending=False).reset_index(drop=True)
+gg = data2.groupby("name")
+data2["Event"] = gg["Event"].fillna(method="ffill")
+
+
+# %%
+data = data2.sort_values(by=["name", "t"])
+# data['Period'] = data['t'] - data['Event']
+
+
+# %%
+
+index = pd.read_excel(path + "IRX6XTPI0009.xls")[["<COL14>", "<CLOSE>"]].rename(
+    columns={"<COL14>": "jalaliDate", "<CLOSE>": "Index"}
+)
+
+pdf.jalaliDate = pdf.jalaliDate.astype(int)
+index = index[index.jalaliDate >= pdf.jalaliDate.min()]
+n = path + "RiskFree rate.xlsx"
+rf = pd.read_excel(n)
+rf = rf.rename(columns={"Unnamed: 2": "Year"})
+rf["YM"] = rf["YM"].astype(str)
+rf["YM"] = rf["YM"] + "00"
+rf["YM"] = rf["YM"].astype(int)
+index["RiskFree"] = np.nan
+index["jalaliDate"] = index["jalaliDate"].astype(int)
+for i in rf.YM:
+    index.loc[index.jalaliDate >= i, "RiskFree"] = (
+        rf.loc[rf["YM"] == i].iloc[0, 1] / 356
+    )
+data = data.merge(index, on="jalaliDate")
+
+# %%
+data = data.sort_values(by=["name", "t"]).reset_index(drop=True)
+gg = data.groupby("name")
+data["Market_return"] = gg["Index"].pct_change(periods=1) * 100
+data["industry_return"] = gg["industry_index"].pct_change(periods=1) * 100
+data = data[~data.Market_return.isnull()]
+#%%
+def divide_to_mean(g):
+    print(g.name, end="\r", flush=True)
+    g = g / g.rolling(60, 1).mean()
+    return g
+
+
+gg = data.groupby("name")
+data["RelVolume"] = gg.volume.apply(divide_to_mean)
+
+#%%
+path1 = r"G:\Economics\Finance(Prof.Heidari-Aghajanzadeh)\Data\\"
+df = pd.read_excel(path1 + "Factors-Daily" + ".xlsx")
+for t in ["SMB", "HML", "Winner_Loser"]:
+    print(t)
+    df[t + "Index"] = df[t] / 100 + 1
+    df[t + "Index"] = df[t + "Index"].rolling(len(df), 1).apply(np.prod)
+
+for t in ["SMB", "HML", "Winner_Loser"]:
+    print(t)
+    mapingdict = dict(zip(df.date, df[t + "Index"]))
+    data[t] = data.date.map(mapingdict)
+    gg = data.groupby("name")
+    data[t] = gg[t].pct_change(periods=1) * 100
+
+#%%
+data.isnull().sum().to_frame()
+data = data.dropna()
+
+n = path + "RawDataBeforeAb2.parquet"
+
+data.to_parquet(n)
+
+
+#%%
+data = pd.read_parquet(path + "RawDataBeforeAb2.parquet")
+#%%
+
+ARdata = pd.DataFrame()
+ARdata = ARdata.append(data)
+ARdata["Return"] = ARdata['return']
+ARdata["ER"] = ARdata["Return"] - ARdata["RiskFree"]
+ARdata["EMR"] = ARdata["Market_return"] - ARdata["RiskFree"]
+ARdata["Industry_return"] = (
+    ARdata["industry_return"] - ARdata["Weight"] * ARdata["Return"]
+) / (1 - ARdata["Weight"])
+ARdata["EIR"] = ARdata["Industry_return"] - ARdata["RiskFree"]
+ARdata.loc[ARdata.Weight == 1.0, "Industry_return"] = np.nan
+ARdata.loc[ARdata.Weight == 1.0, "EIR"] = np.nan
+gg = ARdata.groupby("name")
+# g = gg.get_group("فولاد")
+# dddd = ABnormal(g, 20)
+## Lag
+
+ARdata = gg.apply(ABnormal, Rlag=20).reset_index(drop=True)
+ARdata = ARdata.sort_values(by=["name", "t"])
+ARdata.isnull().sum()
+
+
+#%%
+n = path + "ARdata.parquet"
+
+ARdata.to_parquet(n)
+
+
+
+
+
+# %%
